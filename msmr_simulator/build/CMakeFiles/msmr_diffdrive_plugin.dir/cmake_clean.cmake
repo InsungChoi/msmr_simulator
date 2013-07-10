602280e17680f87cfc0319ef2fe4f18f26984a6d
root <root@MSMR.(none)>
2013-07-10 23:29:08 +0900

    version 1

---
+++ msmr_simulator/build/CMakeFiles/msmr_diffdrive_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/msmr_diffdrive_plugin.dir/src/msmr_diffdrive_plugin.o"
+  "../lib/libmsmr_diffdrive_plugin.pdb"
+  "../lib/libmsmr_diffdrive_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/msmr_diffdrive_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
